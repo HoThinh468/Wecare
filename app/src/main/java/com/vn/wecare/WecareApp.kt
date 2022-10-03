ce4062dc220c3357701188b6952d0a3ea6506058
HoThinh468 <109681288+HoThinh468@users.noreply.github.com>
2022-10-03 22:07:18 +0700

    Update Wecare app

--- app/src/main/java/com/vn/wecare/WecareApp.kt
+++ app/src/main/java/com/vn/wecare/WecareApp.kt
@@ -1,16 +1,28 @@
 package com.vn.wecare
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 
 enum class WecareScreen {
     SignIn,
     SignUp,
+    Home
 }
 
 @Composable
-fun WecareApp() {
+fun WecareApp(
+    modifier: Modifier = Modifier
+) {
     // Create a nav controller
     val navController = rememberNavController()
 
+    // Get current back stack entry
+    val backStackEntry by navController.currentBackStackEntryAsState()
+
+    // Get the name of the current screen
+    val currentScreen =
+        WecareScreen.valueOf(backStackEntry?.destination?.route ?: WecareScreen.Home.name)
 }
